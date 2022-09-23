a875e65441cff865f9aabc534efcf2103e70f656
6302834432 <110374974+6302834432@users.noreply.github.com>
2022-09-23 18:38:17 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,14 @@
+public class swap{
+public static void main(String args[]){
+int x=10,y=20,z;
+System.out.println("before swapping numbers "+x+" "+y);
+/*swapping*/
+z=x;
+x=y;
+y=z;
+System.out.println("After swapping numbers "+x+" "+y);
+System.out.println();
+
+
+}
+}
